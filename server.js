c875c52b3d7552cc9172027585b1b95e75f1a86e
nancy707 <31887214+nancy707@users.noreply.github.com>
2017-09-13 01:05:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/article1',function(req, res) {
+    res.send('article one requested and served here');
+});
+app.get('/article2',function(req, res) {
+    res.send('article two requested and served here');
+});
+app.get('/article3',function(req, res) {
+    res.send('article three requested and served here');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
