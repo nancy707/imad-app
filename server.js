d0ee264e96b1085405d9823aca3906943bcf63c6
nancy707 <31887214+nancy707@users.noreply.github.com>
2017-09-14 23:43:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
 var articles= {
    'article1':{
         title:'ArticleOne',
@@ -61,6 +62,11 @@ return template;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var counter=0;
+app.get('/counter',function(req,res){
+    res.send(counter.toString());
+});
+
 app.get('/:articleName',function(req, res) {
     var atricleName=req.params.atricleName;
     res.send(createTemplate(articles[articleName]));
