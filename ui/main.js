449edb2b0966a6ef03dbbc61f270e15a9df04466
nancy707 <31887214+nancy707@users.noreply.github.com>
2017-09-14 23:07:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var img= document.getElementById('madi');
 marginLeft=0;
 function moveRight(){
     marginLeft=marginLeft + 10;
-    img.style.marginLeft='marginLeft'+'px';
+    img.style.marginLeft=marginLeft+'px';
 }
 img.onclick=function(){
     var interval=setInterval(moveRight,100);
