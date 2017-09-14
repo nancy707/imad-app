d8c8222f9b91c414e2ed65ace131668a6a8f6869
nancy707 <31887214+nancy707@users.noreply.github.com>
2017-09-15 00:14:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,11 @@
 console.log('Loaded!');
-var element = document.getElementById('main-element');
-element.innerHTML='new value 2';
-var img= document.getElementById('madi');
-marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft + 1;
-    img.style.marginLeft=marginLeft+'px';
+var button=document.getElementById('counter');
+var counter=0;
+button.onclick=function(){
+    
+    
+    
+    counter+=1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
 }
-img.onclick=function(){
-    var interval=setInterval(moveRight,50);
-};
