8d83579a6bf96d03e7a59b8275e667e22b263d5a
nancy707 <31887214+nancy707@users.noreply.github.com>
2017-09-14 23:02:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,5 @@ var element = document.getElementById('main-element');
 element.innerHTML='new value 2';
 var img= document.getElementById('madi');
 img.onclick=function(){
-    img.style.marginleft(100px);
-}
+    img.style.marginLeft = '100px';
+};
