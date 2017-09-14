0c773900c9ebbcd9f1858280258ba5bd1a53136d
nancy707 <31887214+nancy707@users.noreply.github.com>
2017-09-14 22:59:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,7 @@
 console.log('Loaded!');
 var element = document.getElementById('main-element');
 element.innerHTML='new value 2';
-console.log(element);
+var img= document.getElementById('madi');
+img.onclick=function(){
+    img.style.marginleft(100px);
+}
